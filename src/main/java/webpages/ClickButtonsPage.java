55c0480f601566c334712aacc142bda7269ae08d
ObinnaOnyeije <obinnaonyeije@yahoo.com>
2020-03-04 23:02:00 -0500

    Added imports for webpage classes

--- src/main/java/webpages/ClickButtonsPage.java
+++ src/main/java/webpages/ClickButtonsPage.java
@@ -1,5 +1,13 @@
 package webpages;
 
+import org.openqa.selenium.WebDriver;
+
+
 public class ClickButtonsPage extends PageObject {
+
+	public ClickButtonsPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/main/java/webpages/ContactUsPage.java
+++ src/main/java/webpages/ContactUsPage.java
@@ -1,5 +1,12 @@
 package webpages;
 
+import org.openqa.selenium.WebDriver;
+
 public class ContactUsPage extends PageObject {
+
+	public ContactUsPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
 
 }
